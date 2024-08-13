2773ab2d4adea6c50779490953f491f26ef91125
seonkyo <tjs0466@naver.com>
2024-08-13 18:21:57 +0900

    Update BOJ 2302.py

--- BOJ/2302.py
+++ BOJ/2302.py
@@ -12,40 +12,37 @@
 # 방법의 가지수는 20억을 넘지 않음
 
 # 접근 방법
-# 고정석을 기준으로 양옆으로 새롭게 구해야함. 칸막이 같은 느낌
-# 특정 값을 사용했는지 판단하는 배열을 하나 두고 관리하면 좋을 것 같음.
-# 고정석은 미리 사용해두고 시작해도 괜찮을 듯.
-# 일단 다 해보고 안되면 갈아끼우는 식으로 해야할지? -> 백트래킹 적용
+# 고정석을 기준으로 각각을 DP로 구하면 됨.
+# 전체 값 대상으로 DP를 구하고 VIP석을 기준으로 나눠서 DP테이블의 값을 참조하면 됨.
 
 import sys
 
-n = int(sys.stdin.readline())
-m = int(sys.stdin.readline())
-vip = [int(sys.stdin.readline()) for _ in range(m)]
+input = sys.stdin.readline
 
-dp = [0] * (n + 1)
-dp[0] = 1
+N = int(input())
+M = int(input())
+vips = [int(input()) for _ in range(M)]
+
+dp = [1] * (N+1)
+# 기저상태 정의
 dp[1] = 1
-if n >= 2:
-    dp[2] = 2
+if N > 1:
+  dp[2] = 2
+  
+# 점화식: dp[n] = dp[n-1] + dp[n-2]
+for i in range(3, N+1):
+    dp[i] = dp[i-1] + dp[i-2]
 
-# 점화식: dp[n] = dp[n - 1] + dp[n - 2]
-for i in range(3, n + 1):
-    dp[i] = dp[i - 1] + dp[i - 2]
-
-answer = 1  # 경우의 수
-if m > 0:
-    pre = 0
-    for j in range(m):
-        # vip[j] - 1 - pre가 음수가 되지 않도록 체크
-        if vip[j] - 1 - pre >= 0:
-            answer *= dp[vip[j] - 1 - pre]
-        pre = vip[j]
+answer = 1
+pre = 0
+for vip in vips:
+    # dp[vips - 1 - pre]는
+		# 4 - 1 - 0 = 3 (첫번쨰 구간은 3개)
+    # 7 - 1 - 4 = 2 (두번째 구간은 2개)
+    answer *= dp[vips - 1 - pre] 
+    pre = vips
     
-    # 마지막 구간 처리
-    if n - pre >= 0:
-        answer *= dp[n - pre]
-else:
-    answer = dp[n]
+# dp[N-pre]는 9 - 7 = 2 (세번째 구간은 3개)
+answer *= dp[N-pre]
 
 print(answer)
