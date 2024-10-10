883f2f927dd446567898935dc9bf002cd077bc3a
seonkyo <tjs0466@naver.com>
2024-10-10 16:25:40 +0900

    BOJ 11657 풀이

---
+++ BOJ/11657.py
@@ -0,0 +1,26 @@
+# https://www.acmicpc.net/problem/11657
+
+N, M = map(int, input().split())
+edges = [tuple(map(int, input().split())) for _ in range(M)]
+
+# 시작점에서 모든 정점까지의 거리 초기화 (무한대)
+distance = [float("inf")] * (N + 1)
+distance[1] = 0
+
+# 정점 V-1번 동안 간선 완화
+for _ in range(N - 1):
+    for u, v, w in edges:
+        if distance[u] != float("inf") and distance[u] + w < distance[v]:
+            distance[v] = distance[u] + w
+
+# 음수 사이클 확인
+for u, v, w in edges:
+    if distance[u] != float("inf") and distance[u] + w < distance[v]:
+        print(-1)
+        exit()
+
+for i, dist in enumerate(distance[2:]):
+    if dist == float("inf"):
+        print(-1)
+    else:
+        print(dist)
