a9ee972218ad9d935c4d622a262cfe4d446055c6
seonkyo <tjs0466@naver.com>
2024-10-31 12:56:18 +0900

    BOJ 11723 풀이

--- BOJ/11723.py
+++ BOJ/11723.py
@@ -1,31 +1,39 @@
-# https://seonkyo.notion.site/Python-0d3378bbb1704111ba34d02ff89f2d29?pvs=4
+# https://www.acmicpc.net/problem/11723
+
+# 비어있는 공집합 S가 주어질때, 아래 연상을 수행하는 프로그램 작성
+# add x: S에 x를 추가한다. (단, S가 x에 이미 있는 경우 연산을 무시)
+# remove x: S에서 x를 제거한다. (단, S에 x가 없는 경우 연산을 무시)
+# check x: S에 x가 있으면 1을, 없으면 0을 출력한다.
+# toggle x: S에 x가 있으면 x를 제거하고, 없으면 x를 추가한다.
+# all: S를 {1, 2, ..., 20}으로 바꾼다.
+# empty: S를 공집합으로 바꾼다.
 
 import sys
 
-m = int(sys.stdin.readline())
-s = set()
+input = sys.stdin.readline
 
-for _ in range(m):
-  temp = sys.stdin.readline().strip().split()
+N = int(input())
+S = set()
 
-  if len(temp) == 1:
-    if temp[0] == "all":
-      S = set([i for i in range(1, 21)])
+for i in range(N):
+    inputString = input().split()
+
+    if len(inputString) == 1:
+        if inputString[0] == "all":
+            S = set([i for i in range(1, 21)])
+        else:
+            S = set()
     else:
-      # empty
-      S = set()
-
-  else:
-    func, x = temp[0], int(temp[1])
-
-    if func == 'add':
-      S.add(x)
-    elif func == "remove":
-      S.discard(x)
-    elif func == 'check':
-      print(1 if x in S else 0)
-    elif func == 'toggle':
-      if x in S:
-        S.discard(x)
-      else:
-        S.add(x)
+        command, number = inputString
+        x = int(number)
+        if command == "add":
+            S.add(x)
+        elif command == "remove":
+            S.discard(x)
+        elif command == "check":
+            print(1 if x in S else 0)
+        elif command == "toggle":
+            if x in S:
+                S.discard(x)
+            else:
+                S.add(x)
