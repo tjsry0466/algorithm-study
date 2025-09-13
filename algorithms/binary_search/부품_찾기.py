d246afb57c9c103070323740b55eb80698ecd75b
seonkyo <tjs0466@naver.com>
2025-09-13 16:44:50 +0900

    부품_찾기 풀이

---
+++ algorithms/binary_search/부품_찾기.py
@@ -0,0 +1,29 @@
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+
+    mid = (start + end) // 2
+
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    else:
+        return binary_search(array, target, mid + 1, end)
+
+    return None
+
+
+n = int(input())
+array = list(map(int, input().split()))
+array.sort()
+
+m = int(input())
+x = list(map(int, input().split()))
+
+for i in x:
+    result = binary_search(array, i, 0, n - 1)
+    if result is not None:
+        print("yes", end=" ")
+    else:
+        print("no", end=" ")
